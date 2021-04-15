327a2bac63c8353cd7ac4aca5eec733ac97b9fc1
Grzegorz Uriasz <gorbak25@gmail.com>
2021-04-15 14:54:17 +0200

    Rebuild

--- truffle-config.js
+++ truffle-config.js
@@ -85,13 +85,14 @@ module.exports = {
     solc: {
       version: "0.8.3",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
       optimizer: {
          enabled: true,
          runs: 20000
       },
+
       //  evmVersion: "byzantium"
-      // }
+      }
     }
   },
 
